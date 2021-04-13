c148a2110be1ff1ccf30fe43289e41fe3234d50e
josadcha <josadcha@heliostech.fr>
2021-04-13 13:54:23 +0300

    Fix: identifier 'fetchConfigs' has already been declared

--- web/public/config.js
+++ web/public/config.js
@@ -1,12 +1,12 @@
 const fetchConfigs = () => {
-    const fetchConfig = new XMLHttpRequest();
+    const fetchConfigRequest = new XMLHttpRequest();
     const hostUrl = window.location.hostname === 'localhost' ? 'http://localhost:9002' : window.location.origin;
 
-    fetchConfig.open('GET', `${hostUrl}/api/v2/sonic/public/config`, false);
-    fetchConfig.send(null);
+    fetchConfigRequest.open('GET', `${hostUrl}/api/v2/sonic/public/config`, false);
+    fetchConfigRequest.send(null);
 
-    if (fetchConfig.status === 200) {
-        window.env = JSON.parse(fetchConfig.responseText);
+    if (fetchConfigRequest.status === 200) {
+        window.env = JSON.parse(fetchConfigRequest.responseText);
     }
 }
 
