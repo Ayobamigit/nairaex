97d40c1ce75e168c94d760cda2bb58c0d8a5c800
akushniruk <akushniruk@openware.com>
2022-02-15 14:09:45 +0200

    Fix: klines fetch

--- web/src/custom/screens/QuickExchange/index.tsx
+++ web/src/custom/screens/QuickExchange/index.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { QuickExchangeContainer } from '../../containers';
 import { setDocumentTitle } from '../../../helpers';
 import { TabPanel } from '../../../components';
+import { setCurrentMarket } from 'src/modules';
 
 export const QuickExchange: React.FC = () => {
     const { formatMessage } = useIntl();
@@ -14,6 +15,10 @@ export const QuickExchange: React.FC = () => {
 
     useEffect(() => {
         setDocumentTitle('Quick Exchange');
+
+        return () => {
+            dispatch(setCurrentMarket(undefined));
+        }
     }, [dispatch]);
 
     const translate = useCallback((id: string) => formatMessage({ id: id }), [formatMessage]);
